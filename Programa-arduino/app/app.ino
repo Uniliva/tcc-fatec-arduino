ee354881d710ce9029f301ff8457eea66a722d9e
Uniliva <uniliva-ap@hotmail.com>
2018-05-30 22:39:52 -0300

    Atualizando o programa deo equipamento

--- Programa-arduino/app/app.ino
+++ Programa-arduino/app/app.ino
@@ -46,111 +46,109 @@ void setup(void)
 
 
 
-void loop(void)
-{
-  //Envia o comando para obter temperaturas
-  sensor.requestTemperatures();
+void loop(void){
+  
+   //Envia o comando para obter temperaturas
+   sensor.requestTemperatures();
 
- // A temperatura em Celsius para o dispositivo 1 no índice 0 (é possivel ligar varios sensores usando a mesma porta do arduino)
-   float leitura=sensor.getTempCByIndex(0);
-   float leitura2=sensor.getTempCByIndex(2);
+   //verifica a energia
+   String temEnergia = verificaEnergia();
+   Serial.println("Tem energia?");     
+   Serial.println(temEnergia);  
+
+   //------------------------------------------------------------------------------------------------------
+    
+   //Coleta os dados
+   float temperaturaSensor1=sensor.getTempCByIndex(0);
 
    //Imprime na serial a varivel que recebe os dados do Sensor
-   Serial.println("Primeiro sensor");  
-   Serial.println(leitura); 
+   Serial.println("Temperatira do primeiro sensor");  
+   Serial.println(temperaturaSensor1); 
+   //envia os dados   
+   enviaDados("https://umonitor-api.herokuapp.com/dados/sensor/1001",temperaturaSensor1, temEnergia);
 
-   Serial.println("Segundo sensor");  
-   Serial.println(leitura2);  
+   //------------------------------------------------------------------------------------------------------
    
-  // salva("https://umonitor-api.herokuapp.com/dados/sensor/1001",leitura);
-  // salva("https://umonitor-api.herokuapp.com/dados/sensor/1002",leitura2); 
+   //Coleta os dados
+   float temperaturaSensor2=sensor.getTempCByIndex(2);
 
-  String energia = verificaEnergia();
+   //Imprime na serial a varivel que recebe os dados do Sensor
+   Serial.println("Temperatira do segundo sensor");  
+   Serial.println(temperaturaSensor2); 
+   //envia os dados   
+   enviaDados("https://umonitor-api.herokuapp.com/dados/sensor/1002",temperaturaSensor2, temEnergia);
 
-  Serial.println("verificando a energia");  
 
-  Serial.println(energia);  
-  
-   
+    //------------------------------------------------------------------------------------------------------
 
+   //Delay de 5 minutos ate a proxima verificação
    //delay(120000); 
     delay(120); 
 
 }
 
 
+//metodo enviaDados para API
+void enviaDados( char urlChar[200], float temp, String temEnergia){  
 
-void salva(char url[200], float temp){  
-    //declarando as variaveis    
-    Serial.println("----------------------->Montando rest");
-    char response[200];
-    char s1[26];
-    String sensor1="{\"temperaturaAtual\":"; 
-    sensor1.concat(temp);
-    sensor1.concat(",\"temEnergia\": true}"); 
-   
-    
-    Serial.println("sensor 1");
-    Serial.println(sensor1);
-       
-    sensor1.toCharArray(s1,200);  
+     //declarando as variaveis    
+    Serial.println("---------------|  Montando rest");
+    char response[10];
+    char bodyChar[50];   
 
-    Serial.println("Rest 1");
-    Serial.println(s1);
-    
+    //Montando body da requisição
+    String body="{\"temperaturaAtual\":"; 
+    body.concat(temp);
+    body.concat(",\"temEnergia\":"); 
+    body.concat(temEnergia); 
+    body.concat("}"); 
+
+    //convertendo de string para char
+    body.toCharArray(bodyChar,50);
+
+    //conectando ao sim800l
     http.configureBearer("movistar.es");   
     http.connect();
 
-    Serial.println("----------------------------->Iniciando o Post");
-    http.post(url,s1, response);  
-    
-    Serial.println("----------------------------->response 1");
-    Serial.println(response);
-        
-    http.disconnect();     
-    Serial.println("----------------------->Post Finalizado");
+    //realizando o request
+    Serial.println("---------------|  Iniciando o Post");
+    http.post(urlChar,bodyChar, response);  
 
+    //obtendo resposta
+    Serial.println("---------------|  Response obtido");
+    Serial.println(response);
+
+    //fechando conexão
+    http.disconnect();     
+    Serial.println("---------------|  Post Finalizado");   
 }
 
+//metodo verifica se tem energia
 String verificaEnergia(){
+  
+    for(int i=10000; i>0; i--){
+      // le o sensor na pino analogico A0 e ajusta o valor lido ja que a saída do sensor é (1023)vcc/2 para corrente =0
+      sensorValue_aux = (analogRead(pinoSensor) -510);
+      // somam os quadrados das leituras.
+      valorSensor += pow(sensorValue_aux,2);
+      delay(1);
+    } 
+    
+    // finaliza o calculo da média quadratica e ajusta o valor lido para volts
+    valorSensor = (sqrt(valorSensor/ 10000)) * voltsporUnidade;
+    
+    // calcula a corrente considerando a sensibilidade do sernsor (185 mV por amper)
+    valorCorrente = (valorSensor/sensibilidade);   
 
-   for(int i=10000; i>0; i--){
-    // le o sensor na pino analogico A0 e ajusta o valor lido ja que a saída do sensor é (1023)vcc/2 para corrente =0
-    sensorValue_aux = (analogRead(pinoSensor) -510);
-    // somam os quadrados das leituras.
-    valorSensor += pow(sensorValue_aux,2);
-    delay(1);
-  }
- 
-  // finaliza o calculo da média quadratica e ajusta o valor lido para volts
-  valorSensor = (sqrt(valorSensor/ 10000)) * voltsporUnidade;
-  // calcula a corrente considerando a sensibilidade do sernsor (185 mV por amper)
-  valorCorrente = (valorSensor/sensibilidade);
+    Serial.println("---------------|  Valor Corrente");   
+     Serial.println(valorCorrente);
 
-
-  //tratamento para possivel ruido
-  //O ACS712 para 30 Amperes é projetado para fazer leitura
-  // de valores alto acima de 0.25 Amperes até 30.
-  // por isso é normal ocorrer ruidos de até 0.20A
-  //por isso deve ser tratado
-  if(valorCorrente <= 0.095){
-    valorCorrente = 0;
-  }
- 
-  valorSensor =0;
- 
- 
-  //Mostra o valor da corrente
-  Serial.print("Corrente : ");
-  // Irms
-  Serial.print(valorCorrente, 3);
- 
- 
-  //Calcula e mostra o valor da potencia
-  Serial.print(" Potencia (Consumo) : ");
-  Serial.print(valorCorrente * tensao);
-  Serial.println(" Watts ");
-
-  return "funfou";
-
+    //validando se tem energia
+    if (valorCorrente < 0.4){
+     //não tem energia
+    return "false";
+    }else {
+     //tem energia
+    return "true";
+    }
 }
